40e483b3648aeff0efb69d7585739a002ffba4fb
Ubuntu <ubuntu@ip-172-31-27-134.ec2.internal>
2019-09-18 18:46:44 +0000

    program add

---
+++ hello_world_circuit_python.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Wajd Mariam
+# Created on: Sep 2019
+# This program is the "Hello, World!" program on the PyBadge
+
+
+def main():
+    # this function prints to the screen and console
+    print("\n\n\n")  # 3 blank lines
+    print("Hello, World!")
+
+    while True:
+        # repeat forever, or you turn it off!
+        pass  # just a place holder
+
+
+if __name__ == "__main__":
+    main()
